ded4fffa443514c15b3331dbbb619d0c4c393ffd
Arild Andersen <arild.andersen@gmail.com>
2026-04-29 10:42:41 +0200

    feat: add robots.txt configuration for search engine indexing

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.bzero.no/sitemap.xml', // Uncomment if you have a sitemap
+  }
+}
